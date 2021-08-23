697d493f7fa9a4aed38ebc559c9049f284d8164c
kgsi <kuusai1998@gmail.com>
2021-08-23 10:35:12 +0900

    feat: update

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,6 +2,5 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "body-max-line-length": [0, "always"],
-    "footer-max-line-length": [0, "always"], // Make sure there is never a max-line-length by disabling the rule
   },
 };
